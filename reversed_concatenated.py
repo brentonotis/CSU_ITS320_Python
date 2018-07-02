12f4196946af49a5c82dcfbd1ae8dbe3fae784d0
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-07-01 23:19:13 -0500

    Update reversed_concatenated.py

--- reversed_concatenated.py
+++ reversed_concatenated.py
@@ -3,14 +3,23 @@ The method should return to the user a concatenation of the string values in rev
 The function is to be called from the main method. In the main method, prompt the user for three strings.
 '''
 
-def string_prompt():
+def prompt():
     prompt = str(input('Enter three strings (separated by spaces): '))
     return prompt
 
-def string_reverse():
-    reversed_string = string_prompt()[::-1] # Call string_prompt (input) function, return reversed input
-    concatenated_string = ''.join(reversed_string.split()) # Split/join string to concatenate/remove white space between text
-    print('The reversed concatenation of your three string values is:', concatenated_string)
+def main():
+    reversed_string = prompt()[::-1]
+    concatenated_string = ''.join(reversed_string.split())
+    print('The reversed concatenation of your string values is:', concatenated_string)
 
-string_reverse()
+if __name__ == '__main__':
+    main()
+
+main()
+
+'''Corrections per instructor feedback:
+      * wrote separate function that will accept three string values
+      * defined main method
+      * called input function in main method
+'''
 
